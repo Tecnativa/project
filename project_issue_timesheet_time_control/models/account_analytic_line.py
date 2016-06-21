ae3dc46a95a149a9c3708eac57c1521e0bfb762f
Antonio Espinosa <antespi@gmail.com>
2016-06-21 10:25:01 +0200

    [FIX] Domain for issue_id when analytic account changes

--- project_issue_timesheet_time_control/models/account_analytic_line.py
+++ project_issue_timesheet_time_control/models/account_analytic_line.py
@@ -8,6 +8,19 @@ from openerp import models, api
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
+    @api.onchange('account_id')
+    def onchange_account_id(self):
+        res = super(AccountAnalyticLine, self).onchange_account_id()
+        domain = res.get('domain', {'issue_id': []})
+        if self.issue_id.project_id.analytic_account_id != self.account_id:
+            self.issue_id = False
+        if self.account_id:
+            project = self.env['project.project'].search(
+                [('analytic_account_id', '=', self.account_id.id)], limit=1)
+            domain = {'issue_id': [('project_id', '=', project.id),
+                                   ('stage_id.fold', '=', False)]}
+        return {'domain': domain}
+
     @api.onchange('issue_id')
     def onchange_issue_id(self):
         if self.issue_id.project_id:
